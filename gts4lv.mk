30d71a3f82db9c84df93fd4836e27133b1ce9850
SamarV-121 <samarvispute121@gmail.com>
2020-05-16 11:37:13 +0530

    gts4lv-common: Import powerstats from bonito
     * Convert to samsung targets
    
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- gts4lv.mk
+++ gts4lv.mk
@@ -238,7 +238,8 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.samsung-libperfmgr
+    android.hardware.power@1.3-service.samsung-libperfmgr \
+    android.hardware.power.stats@1.0-service.samsung
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json    
@@ -267,7 +268,8 @@ PRODUCT_PACKAGES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/google/pixel
 
 # Thermal
 PRODUCT_PACKAGES += \
