08100cec3f90911345cb19178b2b9f19d176db59
Julian Fischer <myst3rion9@gmail.com>
2020-06-11 12:17:05 +0200

    gts4lv-common: Import power-libperfmgr from pixel
    - android-10.0.0_r39
    - hardware/google/pixel
    - powerhint.json from bonito
    - remove qcom power hal

--- gts4lv.mk
+++ gts4lv.mk
@@ -238,7 +238,10 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service.samsung
+    android.hardware.power@1.3-service.samsung-libperfmgr
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json    
 
 # QCOM
 PRODUCT_PACKAGES += \
