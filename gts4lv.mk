11066feec701e622d6f2b5b93e13ea90a2ae8943
Julian Fischer <myst3rion9@gmail.com>
2021-01-05 17:21:48 +0100

    Switch to power-libperfmgr

--- gts4lv.mk
+++ gts4lv.mk
@@ -251,7 +251,10 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service.gts4lv-libperfmgr
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Protobuf
 PRODUCT_PACKAGES += \
@@ -286,7 +289,9 @@ PRODUCT_PACKAGES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel
 
 # Trust HAL
 PRODUCT_PACKAGES += \
